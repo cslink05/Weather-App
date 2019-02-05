e2509ec351db3d617f26e0fbcad036a14e343e1d
Scott Link <c.s.link05@gmail.com>
2019-02-05 19:26:39 +0900

    bg images and mobile responsive

--- app.js
+++ app.js
@@ -9,13 +9,11 @@ window.addEventListener("load", () => {
     let flavorText = document.querySelector('.flavor-text');
     let clockHour = document.querySelector('#hour');
     let clockMinute = document.querySelector('#minute');
-    let clockSecond = document.querySelector('#second');
 
     function clock() {
         const fullDate = new Date();
         let hours = fullDate.getHours();
         let mins = fullDate.getMinutes();
-        let secs = fullDate.getSeconds();
 
         if (hours < 10) {
             hours = "0" + hours;
@@ -25,16 +23,11 @@ window.addEventListener("load", () => {
             mins = "0" + mins;
         }
 
-        if (secs < 10) {
-            secs = "0" + secs;
-        }
-
         clockHour.innerHTML = hours;
         clockMinute.innerHTML = ": " + mins;
-        clockSecond.innerHTML = ": " + secs;
     }
-
-    setInterval(clock, 100);
+    clock();
+    setInterval(clock, 10000);
 
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition((position) => {
@@ -71,52 +64,65 @@ window.addEventListener("load", () => {
                         if (id >= 200 && id <= 232) {
                             weatherIcon.src = "icons/animated/thunder.svg";
                             flavorText.textContent = "be careful out there";
+                            document.body.style.backgroundImage = "url(./img/rainy-day.jpg)";
                         } else if (id >= 300 && id <= 321) {
                             if (clockHour.innerHTML >= 6 && clockHour.innerHTML <= 17) {
                                 weatherIcon.src = "icons/animated/rainy-2.svg";
-                                flavorText.textContent = "might need an umbrella out there today"
+                                flavorText.textContent = "might need an umbrella out there today";
+                                document.body.style.backgroundImage = "url(./img/rainy-day.jpg)";
                             } else {
                                 weatherIcon.src = "icons/animated/rainy-4.svg";
                                 flavorText.textContent = "better bring an umbrella tonight";
+                                document.body.style.backgroundImage = "url(./img/rainy-night.jpg)";
                             }
                         } else if (id >= 500 && id <= 531) {
                             if (clockHour.innerHTML >= 6 && clockHour.innerHTML <= 17) {
                                 weatherIcon.src = "icons/animated/rainy-1.svg";
-                                flavorText.textContent = "it's a wet out there today";
+                                flavorText.textContent = "it's wet out there today";
+                                document.body.style.backgroundImage = "url(./img/rainy-day.jpg)";
                             } else {
                                 weatherIcon.src = "icons/animated/rainy-7.svg";
                                 flavorText.textContent = "a dark and stormy night";
+                                document.body.style.backgroundImage = "url(./img/rainy-night.jpg)";
                             }
                         } else if (id >= 600 && id <= 622) {
                             if (clockHour.innerHTML >= 6 && clockHour.innerHTML <= 17) {
                                 weatherIcon.src = "icons/animated/snowy-1.svg";
                                 flavorText.textContent = "don't forget your coat";
+                                document.body.style.backgroundImage = "url(./img/snowy-day.jpg)";
                             } else {
                                 weatherIcon.src = "icons/animated/snowy-6.svg";
                                 flavorText.textContent = "roads might be slick tonight";
+                                document.body.style.backgroundImage = "url(./img/snowy-night.jpg)";
                             }
                         } else if (id == 800) {
                             if (clockHour.innerHTML >= 6 && clockHour.innerHTML <= 17) {
                                 weatherIcon.src = "icons/animated/day.svg";
                                 flavorText.textContent ="another bright and shiny day";
+                                document.body.style.backgroundImage = "url(./img/sunny-day.jpg)";
                             } else {
                                 weatherIcon.src = "icons/animated/night.svg";
-                                flavorText.textContent = "a calm, relaxing night"
+                                flavorText.textContent = "a calm, relaxing night";
+                                document.body.style.backgroundImage = "url(./img/clear-night.jpg)";
                             }
                         } else if (id >= 801 && id <=803) {
                             if (clockHour.innerHTML >= 6 && clockHour.innerHTML <= 17) {
                                 weatherIcon.src = "icons/animated/cloudy-day-1.svg";
                                 flavorText.textContent = "a few clouds floating around today";
+                                document.body.style.backgroundImage = "url(./img/cloudy-day.jpg)";
                             } else {
                                 weatherIcon.src = "icons/animated/cloudy-night-2.svg";
                                 flavorText.textContent = "a quiet, cloudy night";
+                                document.body.style.backgroundImage = "url(./img/cloudy-night.jpg)";
                             }
                         } else if (id == 804) {
                             weatherIcon.src = "icons/animated/cloudy.svg";
                             flavorText.textContent = "gray doesn't have to be gloomy";
+                            document.body.style.backgroundImage = "url(./img/cloudy-day.jpg)";
                         } else {
                             weatherIcon.src = "icons/animated/day.svg";
                             flavorText.textContent = "have a lovely day";
+                            document.body.style.backgroundImage = "url(./img/sunny-day.jpg)";
                         }
                     }
                     weatherIcons();
